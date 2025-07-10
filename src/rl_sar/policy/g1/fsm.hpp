29d762a4165fb21257b50bd35749128197dc492f
fan-ziqi <fanziqi614@gmail.com>
2025-07-10 20:52:54 +0800

    fix: add `\033[K` to cout

--- src/rl_sar/policy/g1/fsm.hpp
+++ src/rl_sar/policy/g1/fsm.hpp
@@ -73,7 +73,7 @@ public:
                 fsm_command->motor_command.kd[i] = rl.params.fixed_kd[0][i].item<double>();
                 fsm_command->motor_command.tau[i] = 0;
             }
-            std::cout << "\r" << std::flush << LOGGER::INFO << "Getting up " << std::fixed << std::setprecision(2) << rl.running_percent * 100.0f << "%" << std::flush;
+            std::cout << "\r\033[K" << std::flush << LOGGER::INFO << "Getting up " << std::fixed << std::setprecision(2) << rl.running_percent * 100.0f << "%" << std::flush;
         }
     }
 
@@ -142,7 +142,7 @@ public:
                 fsm_command->motor_command.kd[i] = rl.params.fixed_kd[0][i].item<double>();
                 fsm_command->motor_command.tau[i] = 0;
             }
-            std::cout << "\r" << std::flush << LOGGER::INFO << "Getting down "<< std::fixed << std::setprecision(2) << rl.running_percent * 100.0f << "%" << std::flush;
+            std::cout << "\r\033[K" << std::flush << LOGGER::INFO << "Getting down "<< std::fixed << std::setprecision(2) << rl.running_percent * 100.0f << "%" << std::flush;
         }
     }
 
@@ -191,7 +191,7 @@ public:
 
     void Run() override
     {
-        std::cout << "\r" << std::flush << LOGGER::INFO << "RL Controller x:" << rl.control.x << " y:" << rl.control.y << " yaw:" << rl.control.yaw << std::flush;
+        std::cout << "\r\033[K" << std::flush << LOGGER::INFO << "RL Controller x:" << rl.control.x << " y:" << rl.control.y << " yaw:" << rl.control.yaw << std::flush;
 
         torch::Tensor _output_dof_pos, _output_dof_vel;
         if (rl.output_dof_pos_queue.try_pop(_output_dof_pos) && rl.output_dof_vel_queue.try_pop(_output_dof_vel))
@@ -285,7 +285,7 @@ public:
 
     void Run() override
     {
-        std::cout << "\r" << std::flush << LOGGER::INFO << "RL Controller x:" << rl.control.x << " y:" << rl.control.y << " yaw:" << rl.control.yaw << std::flush;
+        std::cout << "\r\033[K" << std::flush << LOGGER::INFO << "RL Controller x:" << rl.control.x << " y:" << rl.control.y << " yaw:" << rl.control.yaw << std::flush;
 
         torch::Tensor _output_dof_pos, _output_dof_vel;
         if (rl.output_dof_pos_queue.try_pop(_output_dof_pos) && rl.output_dof_vel_queue.try_pop(_output_dof_vel))
@@ -384,7 +384,7 @@ public:
         float motion_time = rl.episode_length_buf * rl.params.dt * rl.params.decimation;
         motion_time = fmin(motion_time, rl.motion_length);
         float running_progress = motion_time / rl.motion_length * 100.0f;
-        std::cout << "\r" << std::flush << LOGGER::INFO << "Running progress "<< std::fixed << std::setprecision(2) << running_progress << "%" << std::flush;
+        std::cout << "\r\033[K" << std::flush << LOGGER::INFO << "Running progress "<< std::fixed << std::setprecision(2) << running_progress << "%" << std::flush;
 
         torch::Tensor _output_dof_pos, _output_dof_vel;
         if (rl.output_dof_pos_queue.try_pop(_output_dof_pos) && rl.output_dof_vel_queue.try_pop(_output_dof_vel))
@@ -472,7 +472,7 @@ public:
         float motion_time = rl.episode_length_buf * rl.params.dt * rl.params.decimation;
         motion_time = fmin(motion_time, rl.motion_length);
         float running_progress = motion_time / rl.motion_length * 100.0f;
-        std::cout << "\r" << std::flush << LOGGER::INFO << "Running progress "<< std::fixed << std::setprecision(2) << running_progress << "%" << std::flush;
+        std::cout << "\r\033[K" << std::flush << LOGGER::INFO << "Running progress "<< std::fixed << std::setprecision(2) << running_progress << "%" << std::flush;
 
         torch::Tensor _output_dof_pos, _output_dof_vel;
         if (rl.output_dof_pos_queue.try_pop(_output_dof_pos) && rl.output_dof_vel_queue.try_pop(_output_dof_vel))
@@ -560,7 +560,7 @@ public:
         float motion_time = rl.episode_length_buf * rl.params.dt * rl.params.decimation;
         motion_time = fmin(motion_time, rl.motion_length);
         float running_progress = motion_time / rl.motion_length * 100.0f;
-        std::cout << "\r" << std::flush << LOGGER::INFO << "Running progress "<< std::fixed << std::setprecision(2) << running_progress << "%" << std::flush;
+        std::cout << "\r\033[K" << std::flush << LOGGER::INFO << "Running progress "<< std::fixed << std::setprecision(2) << running_progress << "%" << std::flush;
 
         torch::Tensor _output_dof_pos, _output_dof_vel;
         if (rl.output_dof_pos_queue.try_pop(_output_dof_pos) && rl.output_dof_vel_queue.try_pop(_output_dof_vel))
